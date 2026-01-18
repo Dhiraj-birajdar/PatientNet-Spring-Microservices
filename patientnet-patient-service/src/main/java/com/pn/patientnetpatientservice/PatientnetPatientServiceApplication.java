b4f3d28e7b2b956d2e791bce87813e6673fbdbae
Dhiraj-Birajdar <dhirajbirajdarcs@gmail.com>
2026-01-18 22:32:55 +0530

    initial commit

---
+++ patientnet-patient-service/src/main/java/com/pn/patientnetpatientservice/PatientnetPatientServiceApplication.java
@@ -0,0 +1,13 @@
+package com.pn.patientnetpatientservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PatientnetPatientServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PatientnetPatientServiceApplication.class, args);
+    }
+
+}
---
+++ patientnet-patient-service/src/test/java/com/pn/patientnetpatientservice/PatientnetPatientServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pn.patientnetpatientservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PatientnetPatientServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
